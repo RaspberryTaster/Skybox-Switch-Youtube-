e7aa1443f3316b9039c3427997361b048dd61e28
Raspberry Taster <givemeraspberry@gmail.com>
2021-10-29 15:05:10 +0100

    Initial commit

---
+++ Assets/Scripts/SkyboxManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxManager : MonoBehaviour
+{
+    public Material Day;
+    public Material Night;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeToDay()
+	{
+        RenderSettings.skybox = Day;
+	}
+    public void ChangeToNight()
+	{
+        RenderSettings.skybox = Night;
+	}
+}
